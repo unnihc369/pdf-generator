2c1bd679b1792abd08566acc64bdfc294e2967dc
unnihc <unnihc369@gmail.com>
2023-11-08 18:45:17 +0530

    replace

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,63 @@
-import './App.css'
-import OfferForm from './OfferForm'
+import { useState } from "react";
+import axios from "axios";
+import { saveAs } from "file-saver";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    internshipRole: "Web Developer", // Default value set to "Web Developer"
+    joiningDate: new Date().toISOString().split("T")[0],
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const createAndDownloadOfferLetter = () => {
+    axios
+      .post("http://localhost:5000/create-pdf", formData)
+      .then(() =>
+        axios.get("http://localhost:5000/fetch-pdf", {
+          responseType: "blob",
+        })
+      )
+      .then((res) => {
+        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
+        saveAs(pdfBlob, "OfferLetter.pdf");
+      });
+  };
 
   return (
-    <div className='app'>
-      <OfferForm/>
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Name"
+        name="name"
+        value={formData.name}
+        onChange={handleChange}
+      />
+      <select
+        name="internshipRole"
+        value={formData.internshipRole}
+        onChange={handleChange}
+      >
+        <option value="Web Developer">Web Developer</option>
+        <option value="App Developer">App Developer</option>
+      </select>
+      <input
+        type="date"
+        placeholder="Joining Date"
+        name="joiningDate"
+        value={formData.joiningDate}
+        onChange={handleChange}
+      />
+      <button onClick={createAndDownloadOfferLetter}>
+        Generate Offer Letter
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
