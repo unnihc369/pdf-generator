5ec1622d0c1e3fcff0c30d043798621d0efdb3dd
unnihc <unnihc369@gmail.com>
2023-11-16 18:33:42 +0530

    New code

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,71 @@
-import { useState } from "react";
-import axios from "axios";
-import { saveAs } from "file-saver";
-import "./App.css";
+// src/App.js
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css'; // Import the CSS file for styling
 
 const App = () => {
   const [formData, setFormData] = useState({
-    name: "",
-    internshipRole: "Web Developer", // Default value set to "Web Developer"
-    joiningDate: new Date().toISOString().split("T")[0],
+    name: '',
+    role: '',
+    date: '',
+    phone: '',
+    email: '',
   });
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const createAndDownloadOfferLetter = () => {
-    axios
-      .post("https://pdf-generator-a0y2.onrender.com/create-pdf", formData)
-      .then(() =>
-        axios.get("https://pdf-generator-a0y2.onrender.com/fetch-pdf", {
-          responseType: "blob",
-        })
-      )
-      .then((res) => {
-        const pdfBlob = new Blob([res.data], { type: "application/pdf" });
-        saveAs(pdfBlob, "OfferLetter.pdf");
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:5000/generate-pdf', formData, {
+        responseType: 'blob',
       });
+
+      const blob = new Blob([response.data], { type: 'application/pdf' });
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = 'offer-letter.pdf';
+      link.click();
+    } catch (error) {
+      console.error('Error generating PDF:', error);
+    }
   };
 
   return (
-    <div className="App">
-      <input
-        type="text"
-        placeholder="Name"
-        name="name"
-        value={formData.name}
-        onChange={handleChange}
-      />
-      <select
-        name="internshipRole"
-        value={formData.internshipRole}
-        onChange={handleChange}
-      >
-        <option value="Web Developer">Web Developer</option>
-        <option value="App Developer">App Developer</option>
-      </select>
-      <input
-        type="date"
-        placeholder="Joining Date"
-        name="joiningDate"
-        value={formData.joiningDate}
-        onChange={handleChange}
-      />
-      <button onClick={createAndDownloadOfferLetter}>
-        Generate Offer Letter
-      </button>
+    <div className="container">
+      <h1>Offer Letter Generator</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
+        </label>
+        <label>
+          Role:
+          <select className='input' name="role" value={formData.role} onChange={handleChange} required>
+            <option value="" disabled>
+              Select Role
+            </option>
+            <option value="web-developer">Web Developer</option>
+            <option value="app-developer">App Developer</option>
+          </select>
+        </label>
+        <label>
+          Date of Joining:
+          <input type="date" name="date" value={formData.date} onChange={handleChange} required />
+        </label>
+        <label>
+          Phone Number:
+          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
+        </label>
+        <label>
+          Email:
+          <input type="email" name="email" value={formData.email} onChange={handleChange} required />
+        </label>
+        <button type="submit">Generate PDF</button>
+      </form>
     </div>
   );
 };
